b6a999ed104700ad2421d11eda3ce1728d385909
John Peterson <peterson.glock17@gmail.com>
2021-11-30 20:09:12 -0700

    Built the toggle element from scratch

--- src/components/ToggleSwitch.js
+++ src/components/ToggleSwitch.js
@@ -1,18 +1,24 @@
 
-import classes from './ToggleSwitch.module.css';
+import styles from './ToggleSwitch.module.css';
 
 function ToggleSwitch() {
     return (
-        <div className="flex-col">
-            <datalist id="themes">
-                <option value="1" label="1"></option>
-                <option value="2" label="2"></option>
-                <option value="3" label="3"></option>
-            </datalist>
-            <input type="range" name="theme" id="theme" list="themes" min="1" max="3"/>
+        // <div className="flex-col">
+        //     <datalist id="themes">
+        //         <option value="1" label="1"></option>
+        //         <option value="2" label="2"></option>
+        //         <option value="3" label="3"></option>
+        //     </datalist>
+        //     <input type="range" name="theme" id="theme" list="themes" min="1" max="3"/>
 
-            {/* <input type="radio" name="options" id="option2"/>
-            <input type="radio" name="options" id="option3"/> */}
+        //     {/* <input type="radio" name="options" id="option2"/>
+        //     <input type="radio" name="options" id="option3"/> */}
+        // </div>
+
+        <div>
+            {/* <input type="radio"></input>
+            <input type="radio"></input>
+            <input type="radio"></input> */}
         </div>
     );
 }
