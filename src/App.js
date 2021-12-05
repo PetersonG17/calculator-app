018f62b4e81e7cdfa63148334fe34c73bcde46b1
John Peterson <peterson.glock17@gmail.com>
2021-12-04 18:29:43 -0700

    Working on layout and fonts

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
-import './App.css';
-
 //import Attribution from './components/Attribution';
 import ToggleSwitch from './components/ToggleSwitch';
 
 function App() {
-  return (
-    <ToggleSwitch></ToggleSwitch>
-    // <div className="container p-5">
-    //   <Attribution></Attribution>
-    //   <div className="row">
-    //     <div className="col-2">
-    //       Calc
-    //     </div>
-    //     <div className="col-10 d-flex justify-content-end">
-    //       <div className="d-flex flex-row align-items-end"> {/* Toggle Theme */}
-    //         <div className="pe-3">
-    //           THEME
-    //         </div>
-    //         <div className="d-flex flex-column">
-    //           {/* <div className="d-flex flex-row">
-    //             <div className="p-1">1</div>
-    //             <div className="p-1">2</div>
-    //             <div className="p-1">3</div>
-    //           </div> */}
-    //           <div>
-    //             <ToggleSwitch></ToggleSwitch>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    //   <div className="row">
+  return (    
+    <div className="container p-5">
+      {/* <Attribution></Attribution> */}
+      <div className="row">
+        <div className="col-2">
+          Calculator
+        </div>
+        <div className="col-10 d-flex justify-content-end">
+          <div className="d-flex flex-row align-items-center"> {/* Toggle Theme */}
+            <div className="pe-3">
+              THEME
+            </div>
+            <div className="d-flex flex-column">
+              {/* <div className="d-flex flex-row">
+                <div className="p-1">1</div>
+                <div className="p-1">2</div>
+                <div className="p-1">3</div>
+              </div> */}
+              <div>
+                <ToggleSwitch></ToggleSwitch>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
 
-    //   </div>
-    //   <div className="row">
+      </div>
+      <div className="row">
 
-    //   </div>
-    // </div>
+      </div>
+    </div>
   );
 }
 
