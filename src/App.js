97113ff411d9132bdb6339c7a98bb09ac5b9d5b2
John Peterson <peterson.glock17@gmail.com>
2021-12-28 21:35:38 -0700

    Fixed a little more styling

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ function App() {
   return (    
     <div className="container p-5">
       <div className="row">
-        <div className="col-2 fs-1">
+        <div className="col-2 fs-1 text-white">
           Calculator
         </div>
         <div className="col-10 d-flex justify-content-end">
           <div className="d-flex flex-row align-items-end"> {/* Toggle Theme */}
-            <div className="pe-3">
+            <div className="pe-3 text-white">
               THEME
             </div>
             <ToggleSwitch></ToggleSwitch>
